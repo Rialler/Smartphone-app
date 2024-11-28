cbf7cec033e2c06e639d3ef7b63cf64934de1799
Xavier Riu Penina <rialler1993@gmail.com>
2024-11-28 23:15:57 +0100

    added Smartphone domain entity

---
+++ src/main/java/smartphone/domain/entity/Smartphone.java
@@ -0,0 +1,12 @@
+package smartphone.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class Smartphone {
+
+    private String id;
+    private String name;
+    private String brand;
+    private String price;
+}
