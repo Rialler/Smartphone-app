ac39f8a4617c05f0937c5ba01285897bceedfd7a
Xavier Riu Penina <rialler1993@gmail.com>
2024-11-28 14:09:15 +0100

    boyscout

--- src/main/java/smartphone/Application.java
+++ src/main/java/smartphone/Application.java
@@ -2,14 +2,12 @@ package smartphone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "smartphone")
+@SpringBootApplication(scanBasePackages = "smartphone")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
